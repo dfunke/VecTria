644c0555e233ede284c3c7c682930cd2660fc8a9
Daniel Funke <daniel.funke@kit.edu>
2016-11-17 15:37:38 +0100

    EdgeExtractor concept

---
+++ include/ops/EdgeExtractor.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*
+ * struct EdgeExtractor {
+ *
+ * void getEdge(const SIMPLICES & simplices,
+ *              const BOUNDS & bounds,
+ *              POINT_IDS & edgePoints,
+ *              SIMPLEX_IDS & edgeSimplices);
+ *
+ * }
+ */
+
