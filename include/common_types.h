5642bf74ccd2060912c79f0fadc642319fcb92c8
Daniel Funke <daniel.funke@kit.edu>
2016-11-23 14:38:18 +0100

    id vector

--- include/common_types.h
+++ include/common_types.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#include <vector>
+#include <cstdint>
+
+#include <tbb/concurrent_vector.h>
+
 using tIdType = uint64_t;
 using tHashType = uint32_t;
 using tMarkType = uint8_t;
 using tDimType = uint8_t;
+
+using tIdVector = std::vector<tIdType>;
+using tConcurrentIdVector = tbb::concurrent_vector<tIdType>;
 
 using tIdSet = LP_Set<tIdType, true>;
 using tConcurrentFixedIdSet<tIdType, false>;
@@ -11,5 +19,5 @@ using tConcurrentGrowingIdSet = GrowingHashTable <Concurrent_LP_Set<tIdType, tru
 using tConcurrentGrowingIdSetHandle = GrowingHashTableHandle <Concurrent_LP_Set<tIdType, true>>;
 
 using tFacetteLookUp = LP_MultiMap<tHashType, tIdType>;
-using tConcurrentGrowingFacetteLookUp = GrowingHashTable<Concurrent_LP_MultiMap<tHashType, tIdType>>;
-using tConcurrentGrowingFacetteLookUpHandle= GrowingHashTableHandle<Concurrent_LP_MultiMap<tHashType, tIdType>>;
+using tConcurrentGrowingFacetteLookUp = GrowingHashTable <Concurrent_LP_MultiMap<tHashType, tIdType>>;
+using tConcurrentGrowingFacetteLookUpHandle= GrowingHashTableHandle <Concurrent_LP_MultiMap<tHashType, tIdType>>;
