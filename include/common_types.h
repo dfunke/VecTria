26d1edfe3c08e3328b9bb4c75666f736e08971bb
Daniel Funke <daniel.funke@kit.edu>
2016-11-23 16:15:10 +0100

    fix typedef

--- include/common_types.h
+++ include/common_types.h
@@ -14,7 +14,7 @@ using tIdVector = std::vector<tIdType>;
 using tConcurrentIdVector = tbb::concurrent_vector<tIdType>;
 
 using tIdSet = LP_Set<tIdType, true>;
-using tConcurrentFixedIdSet<tIdType, false>;
+using tConcurrentFixedIdSet = Concurrent_LP_Set<tIdType, false>;
 using tConcurrentGrowingIdSet = GrowingHashTable <Concurrent_LP_Set<tIdType, true>>;
 using tConcurrentGrowingIdSetHandle = GrowingHashTableHandle <Concurrent_LP_Set<tIdType, true>>;
 
