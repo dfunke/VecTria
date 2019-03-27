0c46e70cfb75b5144572c1aa1943b65b9c15fcf3
Daniel Funke <funke@kit.edu>
2019-03-27 15:27:01 +0100

    restructure

--- includes/Datastructures.h
+++ includes/Datastructures.h
@@ -1,11 +1,3 @@
-//
-//  Header.h
-//  delaunay
-//
-//  Created by Daniel Funke on 21.03.19.
-//  Copyright © 2019 Daniel Funke. All rights reserved.
-//
-
 #pragma once
 
 #include <vector>
@@ -19,99 +11,205 @@ using tFloatVector = std::vector<Precision>;
 
 using tIndexVector = std::vector<tIndexType>;
 
-template<tDimType D, typename Precision>
-class PointArray {
-    
-public:
-    std::array<tFloatVector<Precision>, D> coords;
-    
-public:
-    
-    void ensure(const tIndexType & i){
-        for(tDimType d = 0; d < D+1; ++d){
-            if(coords[d].size() < i){
-                coords[d].resize(i);
-            }
-        }
-    }
-    
-};
-
-template <tDimType D, typename Precision>
+template<tDimType D, typename Precision, class PointArray>
 class Point {
-    
+
 private:
-    
-    PointArray<D, Precision> & m_pointArray;
+
+    PointArray &m_pointArray;
     tIndexType m_idx;
-    
+
 public:
-    
-    Point(PointArray<D, Precision> & pointArray, tIndexType & idx)
-    : m_pointArray(pointArray), m_idx(idx)
-    {}
-    
-    const Precision & operator[](const tDimType & i) const {
-        return m_pointArray.coords[i][m_idx];
+
+    Point(PointArray &pointArray, const tIndexType &idx)
+            : m_pointArray(pointArray), m_idx(idx) {}
+
+    const Precision &operator[](const tDimType &i) const {
+        return m_pointArray(m_idx, i);
     }
-    
-    Precision & operator[](const tDimType & i) {
-        return m_pointArray.coords[i][m_idx];
+
+    Precision &operator[](const tDimType &i) {
+        return m_pointArray(m_idx, i);
     }
 };
 
-template <tDimType D>
-class SimplexArray {
-    
+template<tDimType D, typename Precision>
+class PointAoA {
+
+private:
+    std::array<tFloatVector<Precision>, D> coords;
+
+    using tPoint = Point<D, Precision, PointAoA<D, Precision>>;
+
 public:
-    std::array<tIndexVector, D+1> vertices;
-    std::array<tIndexVector, D+1> neighbors;
-    
-public:
-    
-    void ensure(const tIndexType & i){
-        for(tDimType d = 0; d < D+1; ++d){
-            if(vertices[d].size() < i){
-                vertices[d].resize(i);
-            }
-            
-            if(neighbors[d].size() < i){
-                neighbors[d].resize(i);
+
+    const Precision &operator()(const tIndexType &i, const tDimType &d) const {
+        return coords[d][i];
+    }
+
+    Precision &operator()(const tIndexType &i, const tDimType &d) {
+        return coords[d][i];
+    }
+
+    void ensure(const tIndexType &i) {
+        for (tDimType d = 0; d < D + 1; ++d) {
+            if (coords[d].size() < i + 1) {
+                coords[d].resize(i + 1);
             }
         }
     }
+
+    tPoint get(const tIndexType &i) {
+        return tPoint(*this, i);
+    }
+
 };
 
-template <tDimType D>
-class Simplex {
-    
+template<tDimType D, typename Precision>
+class PointPA {
+
 private:
-    
-    SimplexArray<D> & m_simplexArray;
-    tIndexType m_idx;
-    
+    tFloatVector<Precision> coords;
+
+    using tPoint = Point<D, Precision, PointPA<D, Precision>>;
+
 public:
-    
-    Simplex(SimplexArray<D> & simplexArray, tIndexType & idx)
-    : m_simplexArray(simplexArray), m_idx(idx)
-    {
+
+    const Precision &operator()(const tIndexType &i, const tDimType &d) const {
+        return coords[D * i + d];
+    }
+
+    Precision &operator()(const tIndexType &i, const tDimType &d) {
+        return coords[D * i + d];
+    }
+
+    void ensure(const tIndexType &i) {
+        if (coords.size() < D * (i + 1)) {
+            coords.resize(D * (i + 1));
+        }
+    }
+
+    tPoint get(const tIndexType &i) {
+        return tPoint(*this, i);
+    }
+
+};
+
+template<tDimType D, class SimplexArray>
+class Simplex {
+
+private:
+
+    SimplexArray &m_simplexArray;
+    tIndexType m_idx;
+
+public:
+
+    Simplex(SimplexArray &simplexArray, const tIndexType &idx)
+            : m_simplexArray(simplexArray), m_idx(idx) {
         m_simplexArray.ensure(m_idx);
     }
-    
-    const tIndexType & vertex(const tDimType & i) const {
+
+    const tIndexType &vertex(const tDimType &i) const {
         return m_simplexArray.vertices[i][m_idx];
     }
-    
-    tIndexType & vertex(const tDimType & i) {
+
+    tIndexType &vertex(const tDimType &i) {
         return m_simplexArray.vertices[i][m_idx];
     }
-    
-    const tIndexType & neighbor(const tDimType & i) const {
+
+    const tIndexType &neighbor(const tDimType &i) const {
         return m_simplexArray.neighbors[i][m_idx];
     }
-    
-    tIndexType & neighbor(const tDimType & i) {
+
+    tIndexType &neighbor(const tDimType &i) {
         return m_simplexArray.neighbors[i][m_idx];
     }
-    
+
+};
+
+template<tDimType D>
+class SimplexAoA {
+
+private:
+    std::array<tIndexVector, D + 1> vertices;
+    std::array<tIndexVector, D + 1> neighbors;
+
+    using tSimplex = Simplex<D, SimplexAoA<D>>;
+
+public:
+
+    const tIndexType &vertex(const tIndexType &i, const tDimType &d) const {
+        return vertices[d][i];
+    }
+
+    tIndexType &vertex(const tIndexType &i, const tDimType &d) {
+        return vertices[d][i];
+    }
+
+    const tIndexType &neighbor(const tIndexType &i, const tDimType &d) const {
+        return neighbors[d][i];
+    }
+
+    tIndexType &neighbor(const tIndexType &i, const tDimType &d) {
+        return neighbors[d][i];
+    }
+
+    void ensure(const tIndexType &i) {
+        for (tDimType d = 0; d < D + 1; ++d) {
+            if (vertices[d].size() < i + 1) {
+                vertices[d].resize(i + 1);
+            }
+
+            if (neighbors[d].size() < i + 1) {
+                neighbors[d].resize(i + 1);
+            }
+        }
+    }
+
+    tSimplex get(const tIndexType &i) {
+        return tSimplex(*this, i);
+    }
+};
+
+template<tDimType D>
+class SimplexPA {
+
+private:
+    tIndexVector vertices;
+    tIndexVector neighbors;
+
+    using tSimplex = Simplex<D, SimplexPA<D>>;
+
+public:
+
+    const tIndexType &vertex(const tIndexType &i, const tDimType &d) const {
+        return vertices[(D + 1) * i + d];
+    }
+
+    tIndexType &vertex(const tIndexType &i, const tDimType &d) {
+        return vertices[(D + 1) * i + d];
+    }
+
+    const tIndexType &neighbor(const tIndexType &i, const tDimType &d) const {
+        return neighbors[(D + 1) * i + d];
+    }
+
+    tIndexType &neighbor(const tIndexType &i, const tDimType &d) {
+        return neighbors[(D + 1) * i + d];
+    }
+
+    void ensure(const tIndexType &i) {
+        if (vertices.size() < (D + 1) * (i + 1)) {
+            vertices.resize((D + 1) * (i + 1));
+        }
+
+        if (neighbors.size() < (D + 1) * (i + 1)) {
+            neighbors.resize((D + 1) * (i + 1));
+        }
+    }
+
+    tSimplex get(const tIndexType &i) {
+        return tSimplex(*this, i);
+    }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,49 @@
 // Created by funke on 11/23/16.
 //
 
-#include "../includes/Datastructures.h"
+#include <random>
+#include <functional>
+
+#include "Datastructures.h"
+#include "Predicates.h"
+
+#define SEED 1986
+
+template<tDimType D, typename Precision, class PointArray>
+void generatePoints(PointArray &pa, const tIndexType &n) {
+
+    std::mt19937 generator(SEED);
+    std::uniform_real_distribution<Precision> distribution;
+    auto rand = std::bind(distribution, generator);
+
+    pa.ensure(n);
+    for (tIndexType i = 0; i < n; ++i) {
+        auto p = pa.get(i);
+
+        for (tDimType d = 0; d < D; ++d) {
+            p[d] = rand();
+        }
+    }
+}
 
 #define D 3
 #define Precision double
+#define N 1e4
 
 int main() {
 
-    PointArray<D, Precision> points;
-    SimplexArray<D> simplices;
-    
+    PointAoA<D, Precision> points_aoa;
+    generatePoints<D, Precision>(points_aoa, N);
+
+    PointPA<D, Precision> points_pa;
+    generatePoints<D, Precision>(points_pa, N);
+
+    SimplexAoA<D> simplices_aoa;
+    SimplexPA<D> simplices_pa;
+
+    __attribute__((unused)) auto s_aoa = simplices_aoa.get(0);
+    __attribute__((unused)) auto s_pa = simplices_pa.get(0);
+
     return 0;
 
 }
